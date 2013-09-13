2c5206d105c4593f45db6e131854a7e03575c410
sizuhiko <k-kishida@esm.co.jp>
2013-09-13 22:20:42 +0900

    routeの修正を元に戻した

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -33,12 +33,6 @@
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 
 /**
- * REST access
- */
-	Router::mapResources('recipes');
-	Router::parseExtensions('json');
-
-/**
  * Load all plugin routes. See the CakePlugin documentation on
  * how to customize the loading of plugin routes.
  */
